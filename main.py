f7839ecbc4e6f2f85613371f5c8afd1eeddf0bae
Pranay Mittal <pranaym12@gmail.com>
2019-04-10 01:10:43 -0500

    pretty up the code

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # N2S: print statement: #app.logger.error('hello world')
 
-#PIP: To use requests and requests_oauthlib
-#pip install requests requests_oauthlib
 from requests_oauthlib import OAuth2Session 
-import requests #from maraujop
+import requests #pip install requests requests_oauthlib
 
-# from requests_oauthlib import OAuth2BearerToken #bearerToken=marujop
-#PIP: must use pip install flask
-from flask import Flask, request, redirect, session, url_for
+from flask import Flask, request, redirect, session, url_for #pip install flask
 from flask.json import jsonify
 import os
 from flask import render_template #for Home and About
@@ -60,32 +56,17 @@ def callback():
     in the redirect URL. We will use that to obtain an access token.
     """
 
-    #STEP 1: get token
-    #A: send a POST request to https://www.eventbrite.ocm/oauth/token
-    #B: This POST must contain the following urlencoded data, along with a Content-type: 
-    #[Content-type!] application/x-www-form-urlencoded header:
-    #[URL-encoded-Data!] code=THE_USERS_AUTH_CODE&client_secret=YOUR_CLIENT_SECRET&client_id=YOUR_API_KEY&grant_type=authorization_code
-    code = request.args.get("code") #right now code gets the code
+    #STEP 1: get token through post request
+    code = request.args.get("code") #right now code gets the code from the URL
     url = "https://www.eventbrite.com/oauth/token"
-    payload = "code="+code+"&client_id=3BXHKSKAFIL2HZQ7D6&client_secret=RDJMSHVG2RRBMPACDMRAQXL4OZ3MXWMU4TW56NK3KDDNHSV5JJ&grant_type=authorization_code"
+    payload = "code="+code+"&client_id="+client_id+"&client_secret="+client_secret+"&grant_type=authorization_code"
     headers = {'Content-Type': "application/x-www-form-urlencoded",}
-
+    #make the post request!
     response = requests.request("POST", url, data=payload, headers=headers)
 
-    return str(response.json()["access_token"])
+    return str(response.json()["access_token"]) #display the token!
 
     #STEP 2: redirect to https://www.eventbriteapi.com/v3/users/me/?token=SESXYS4X3FJ5LHZRWGKQ
-
-    
-    ###eventbrite = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, state=session['oauth_state']) #comm4Pot
-    #^redirect_uri may not be nec
-    # eventbrite = OAuth2Session(client_id) #not enough inputs in this one
-    ###token = eventbrite.fetch_token(token_url, client_secret=client_secret,authorization_response=request.url)
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
-    ###session['oauth_token'] = token
-    ###return redirect(url_for('.profile')) #this was in the copypasta
 
 
 @app.route("/profile", methods=["GET"])
